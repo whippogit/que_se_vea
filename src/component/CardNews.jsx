81f551cfac83f7fbdd0fc59e6e3e5160d5ab1b6a
Whippo <whippocode@gmail.com>
2022-12-12 16:09:22 -0300

    update

--- src/component/CardNews.jsx
+++ src/component/CardNews.jsx
@@ -4,29 +4,26 @@ import { Link } from "react-router-dom";
 function CardNews({ news }) {
   return (
     <div className="container">
-      <div>
-        {news.map((newsapi) => (
-          <Link
-            to={"/new/" + newsapi._id + newsapi.title}
-            key={newsapi._id}
-            title={newsapi.title}
-            className="cardContainer"
-          >
-            <img src={newsapi.image_url} alt={newsapi.title} />
-            <div className="categoria">
-              <h6>{newsapi.category}</h6>
-            </div>
-            <div className="titulo">{newsapi.title}</div>
-            <p className="description">{newsapi.description}</p>
-            <div className="footer-card">
-              
-              <div className="by">Dirección de Información Pública</div>
-            </div>
-            <div className="date">11 de Diciembre 2022</div>
-          </Link>
-        ))},
-        
-      </div>
+      {news.map((newsapi) => (
+        <Link
+          to={"/new/" + newsapi._id + newsapi.title}
+          key={newsapi._id}
+          title={newsapi.title}
+          className="cardContainer"
+        >
+          <img src={newsapi.image_url} alt={newsapi.title} />
+          <div className="categoria">
+            <h6>{newsapi.category}</h6>
+          </div>
+          <div className="titulo">{newsapi.title}</div>
+          <p className="description">{newsapi.description}</p>
+          <div className="footer-card">
+            <div className="by">Dirección de Información Pública</div>
+          </div>
+          <div className="date">11 de Diciembre 2022</div>
+        </Link>
+      ))}
+      ,
     </div>
   );
 }
--- src/services/News.js
+++ src/services/News.js
@@ -1,4 +1,4 @@
-const Api = "http://localhost:3000/api";
+const Api = "https://appwhippo.onrender.com/api";
 
 export async function getAllNews() {
   try {
