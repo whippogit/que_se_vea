4915b3ee5cd3b9fa0f921b6b316db9ab8260495f
whippo <whippocode@gmail.com>
2023-02-04 19:07:09 -0300

    delete img dir

--- src/component/BannerBottom.jsx
+++ src/component/BannerBottom.jsx
@@ -1,7 +1,7 @@
 function BannerBottom() {
   return (
     <>
-      <img className="w-50 m-3 rounded" src="/src/img/redes_gob_900x300px.gif" alt="Corrientes Somos Todos" />
+      <img className="w-50 m-3 rounded" src="https://res.cloudinary.com/dm4wfkipp/image/upload/v1675546467/redes_gob_900x300px_ce87lw.gif" alt="Corrientes Somos Todos" />
     </>
   );
 }
