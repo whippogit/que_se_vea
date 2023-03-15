1095501641ec1a2f8432cdcf7da185c8e9bb68a2
whippo <whippocode@gmail.com>
2023-03-15 14:23:03 -0300

    change size alert

--- src/component/Alert.jsx
+++ src/component/Alert.jsx
@@ -5,7 +5,7 @@ function Alert() {
     <a href="https://www.corrientes.gob.ar/1432/salud-comunica-cronograma-de-las-vacunas-contra-covid-19-y-gripe" target={"_blank"}>
       <img
         src="https://res.cloudinary.com/dm4wfkipp/image/upload/v1677637860/vacunate_c5ggrp.jpg"
-        className="w-100 m-2 rounded"
+        className="w-75 m-2 rounded"
       />
     </a>
   );
